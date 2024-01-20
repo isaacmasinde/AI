b02070217c31ed7b37aa10373ae6a27f2eaf8477
isaacmasinde <isaacmasinde2019@gmail.com>
2024-01-20 21:34:19 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
 completion = client.chat.completions.create(
   model="local-model", 
   messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
+    {"role": "system", "content": "answer stating with abraham linch"},
+    {"role": "user", "content": "Write the code to extract the files in python"}
   ],
   temperature=0.7,
 )
